fed87548400de1345372b417d021be45b05ec6dd
illsk1lls <gene.rovinsky@gmail.com>
2023-12-28 01:30:22 -0500

    Update ZipRipper.cmd
    added RAR and 7z support to the script

--- ZipRipper.cmd
+++ ZipRipper.cmd
@@ -1,5 +1,7 @@
 @ECHO OFF&TITLE Please Wait...&SET/A GPU=0&CALL :CHECKCOMPAT&IF NOT "%~2"=="" ECHO Multiple files are not supported, Please drop one file at a time.&ECHO.&PAUSE&EXIT
-IF "%~1"=="" (ECHO Drop a password protected ZIP file onto the script to begin...&ECHO.&PAUSE&EXIT) ELSE (IF /I NOT "%~x1"==".zip" ECHO Only ZIP archives are supported...&ECHO.&PAUSE&EXIT)
+SET "SUPPORTED=ZIP,RAR,7z"
+IF "%~1"=="" (ECHO Drop a password protected %SUPPORTED% file onto the script to begin...&ECHO.&PAUSE&EXIT) ELSE (FOR %%# IN (%SUPPORTED%) DO IF /I "%~x1"==".%%#" SET/a GO=1)
+(IF %GO% NEQ 1 ECHO Unsupported file extension. Supported extensions are: %SUPPORTED%&ECHO.&PAUSE&EXIT)&SET "FILETYPE=%~x1"
 >nul 2>&1 reg add hkcu\software\classes\.ZipRipper\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\"& call \"%%2\" %%3"& set _= %*
 >nul 2>&1 fltmc|| if "%f0%" neq "%~f0" (cd.>"%ProgramData%\elevate.ZipRipper" & start "%~n0" /high "%ProgramData%\elevate.ZipRipper" "%~f0" "%_:"=""%" & exit /b)
 >nul 2>&1 reg delete hkcu\software\classes\.ZipRipper\ /f &>nul 2>&1 del %ProgramData%\elevate.ZipRipper /f /q
@@ -15,16 +17,8 @@ PING -n 1 "google.com" | findstr /r /c:"[0-9] *ms">nul
 IF NOT %errorlevel%==0 ECHO.&ECHO Internet connection not detected, the latest JtR is needed to proceed...&ECHO.&PAUSE&(GOTO) 2>nul&del "%~f0" /F /Q>nul&EXIT
 IF EXIST "%ProgramData%\JtR" >nul 2>&1 RD "%ProgramData%\JtR" /S /Q
 CALL :GETJTRREADY
-PUSHD "%ProgramData%\JtR\run"&CLS
-IF %~z1 GEQ 200000000 (ECHO Creating Password Hash - This can take a few minutes on large files...) ELSE (ECHO Creating Password Hash...)
-zip2john "%~1">"%ProgramData%\JtR\run\pwhash" 2>nul
-SET "FLAG="
-IF %GPU% EQU 1 (
-FOR /F "tokens=2 delims=$" %%# IN (pwhash) DO (
-IF "%%#"=="pkzip" SET "FLAG=--format=PKZIP"
-IF "%%#"=="zip2" SET "FLAG=--format=ZIP-opencl"
-)
-)
+PUSHD "%ProgramData%\JtR\run"
+CALL :GO%FILETYPE% %1
 SETLOCAL ENABLEDELAYEDEXPANSION
 CLS&ECHO Running JohnTheRipper...&ECHO.
 john pwhash !FLAG!
@@ -33,11 +27,12 @@ ECHO.&PAUSE
 POPD&RD "%ProgramData%\JtR" /S /Q>nul&(GOTO) 2>nul&DEL "%~f0"/F /Q>nul&EXIT
 :GETJTRREADY
 CLS&ECHO Getting Decompression Tools...
-POWERSHELL -nop -c "Invoke-WebRequest -Uri https://www.7-zip.org/a/7zr.exe -o '%~dp07zr.exe'"
-CLS&ECHO Getting Decryption Tools...
-POWERSHELL -nop -c "Start-BitsTransfer -Priority Foreground -Source https://github.com/openwall/john-packages/releases/download/jumbo-dev/winX64_1_JtR.7z -Destination '%~dp0winX64_1_JtR.7z'"
->nul 2>&1 "%~dp07zr.exe" x -y "winX64_1_JtR.7z"
->nul 2>&1 DEL winX64_1_JtR.7z /F /Q&>nul 2>&1 DEL 7zr.exe /F /Q
+POWERSHELL -nop -c "Invoke-WebRequest -Uri https://www.7-zip.org/a/7zr.exe -o '%~dp07zr.exe'"; "Invoke-WebRequest -Uri https://www.7-zip.org/a/7z2300-extra.7z -o '%~dp07zExtra.7z'"
+IF /I "%FILETYPE%"==".7z" (CLS&ECHO Getting Decryption Tools, Be patient...&SET "EXTRA=;Start-BitsTransfer -Priority Foreground -Source https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_5380_5361/strawberry-perl-5.38.0.1-64bit-portable.zip -Destination '%~dp0\perlportable.zip'") ELSE (CLS&ECHO Getting Decryption Tools...&SET "EXTRA=")
+POWERSHELL -nop -c "Start-BitsTransfer -Priority Foreground -Source https://github.com/openwall/john-packages/releases/download/jumbo-dev/winX64_1_JtR.7z -Destination '%~dp0winX64_1_JtR.7z'%EXTRA%"
+>nul 2>&1 "%~dp07zr.exe" x -y "%~dp0winX64_1_JtR.7z"&>nul 2>&1 "%~dp07zr.exe" x -y "%~dp07zExtra.7z" -o"%~dp0JtR\"
+IF /I "%FILETYPE%"==".7z" CLS&ECHO Extracting Decryption Dependencies, this will take a moment...&"%~dp0JtR\7za.exe" x -y "%~dp0perlportable.zip" -o"%~dp0JtR\run">nul&>nul 2>&1 DEL "%~dp0perlportable.zip" /F /Q
+>nul 2>&1 DEL "%~dp0winX64_1_JtR.7z" /F /Q&>nul 2>&1 DEL "%~dp07zr.exe" /F /Q&>nul 2>&1 DEL "%~dp07zExtra.7z" /F /Q
 IF %GPU% EQU 1 >nul 2>&1 COPY /Y "%WinDir%\System32\OpenCL.dll" "%ProgramData%\JtR\run\cygOpenCL-1.dll"
 EXIT/b
 :CHECKCOMPAT
@@ -47,4 +42,32 @@ POWERSHELL -nop -c "Get-WmiObject -Class Win32_OperatingSystem | Format-List -Pr
 ::IF %errorlevel% == 0 Win 11 Stuff here...
 FOR /F "usebackq skip=1 tokens=2,3" %%# in (`WMIC path Win32_VideoController get Name ^| findstr "."`) DO (IF /I "%%#"=="GeForce" SET/A GPU=1)&(IF /I "%%#"=="Quadro" SET/A GPU=1)&(IF /I "%%# %%$"=="Radeon RX" SET/A GPU=1)&(IF /I "%%# %%$"=="Radeon Pro" SET/A GPU=1)
 IF NOT EXIST "%WinDir%\System32\OpenCL.dll" SET/A GPU=0
+EXIT/b
+:GO.ZIP
+CLS&IF %~z1 GEQ 200000000 (ECHO Creating Password Hash - This can take a few minutes on large files...) ELSE (ECHO Creating Password Hash...)
+zip2john "%~1">"%ProgramData%\JtR\run\pwhash" 2>nul
+SET "FLAG="
+IF %GPU% EQU 1 (
+FOR /F "tokens=2 delims=$" %%# IN (pwhash) DO (
+IF "%%#"=="pkzip" SET "FLAG=--format=PKZIP"
+IF "%%#"=="zip2" SET "FLAG=--format=ZIP-opencl"
+)
+)
+EXIT/b
+:GO.RAR
+CLS&IF %~z1 GEQ 200000000 (ECHO Creating Password Hash - This can take a few minutes on large files...) ELSE (ECHO Creating Password Hash...)
+rar2john "%~1">"%ProgramData%\JtR\run\pwhash" 2>nul
+SET "FLAG="
+IF %GPU% EQU 1 (
+FOR /F "tokens=2 delims=$" %%# IN (pwhash) DO (
+IF "%%#"=="rar" SET "FLAG=--format=rar-opencl"
+IF "%%#"=="rar5" SET "FLAG=--format=RAR5-opencl"
+)
+)
+EXIT/b
+:GO.7z
+CLS&IF %~z1 GEQ 200000000 (ECHO Creating Password Hash - This can take a few minutes on large files...) ELSE (ECHO Creating Password Hash...)
+CALL portableshell.bat 7z2john.pl "%~1">"%ProgramData%\JtR\run\pwhash"
+SET "FLAG="
+IF %GPU% EQU 1 SET "FLAG=--format=7z-opencl"
 EXIT/b
