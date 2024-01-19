d01429c8e63271bcf2cbbd82de40f3c76478b767
illsk1lls <gene.rovinsky@gmail.com>
2024-01-18 19:37:21 -0500

    Update zr-offline-builder.ps1

--- .resources/zr-offline-builder.ps1
+++ .resources/zr-offline-builder.ps1
@@ -10,21 +10,21 @@ Add-Type -AssemblyName PresentationFramework, System.Drawing, System.Windows.For
 	<Canvas>
     <TextBlock Name="Info" Text=" Preparing Download..." Foreground="#eeeeee" FontWeight="Bold">
     </TextBlock>
-		<ProgressBar Canvas.Top="20" Width="200" Height="20" Name="DownloadProgress"/>
-		<ProgressBar Canvas.Top="44" Width="200" Height="20" Name="DownloadProgressTotal"/>
+		<ProgressBar Canvas.Top="20" Width="200" Height="20" Name="Progress"/>
+		<ProgressBar Canvas.Top="44" Width="200" Height="20" Name="Progress2"/>
 	</Canvas>
 </Window>'
 
-$reader2=(New-Object System.Xml.XmlNodeReader $xaml2);$window2=[Windows.Markup.XamlReader]::Load($reader2)
+$reader=(New-Object System.Xml.XmlNodeReader $xaml2);$form=[Windows.Markup.XamlReader]::Load($reader)
 
-$window2.Add_Closing({[System.Windows.Forms.Application]::Exit();Stop-Process $pid})
+$form.Add_Closing({[System.Windows.Forms.Application]::Exit();Stop-Process $pid})
 
-$progressBar=$window2.FindName("DownloadProgress")
-$progressBarTotal=$window2.FindName("DownloadProgressTotal")
-$info=$window2.FindName("Info")
+$progressBar=$form.FindName("Progress")
+$progressTotal=$form.FindName("Progress2")
+$info=$form.FindName("Info")
 
 function Update-Gui (){
-    $window.Dispatcher.Invoke([Windows.Threading.DispatcherPriority]::Background, [action]{})
+    $form.Dispatcher.Invoke([Windows.Threading.DispatcherPriority]::Background, [action]{})
 }
 
 function Build () {
@@ -33,62 +33,63 @@ function Build () {
 	}
 	New-Item -Path ".\" -Name "ztmp" -ItemType "directory"
 	downloadFile "https://raw.githubusercontent.com/illsk1lls/ZipRipper/main/.resources/zipripper.png" ".\ztmp\zipripper.png";
-	$progressBarTotal.Value = 1
+	$progressTotal.Value=1
 	Update-Gui
 	downloadFile "https://www.7-zip.org/a/7zr.exe" ".\ztmp\7zr.exe";
-	$progressBarTotal.Value = 15
+	$progressTotal.Value=15
 	Update-Gui
 	downloadFile "https://www.7-zip.org/a/7z2301-x64.exe" ".\ztmp\7z2301-x64.exe";
-	$progressBarTotal.Value = 27
+	$progressTotal.Value=27
 	Update-Gui
 	downloadFile "https://www.7-zip.org/a/7z2300-extra.7z" ".\ztmp\7zExtra.7z";
-	$progressBarTotal.Value = 45
+	$progressTotal.Value=45
 	Update-Gui
 	downloadFile "https://github.com/openwall/john-packages/releases/download/jumbo-dev/winX64_1_JtR.7z" ".\ztmp\winX64_1_JtR.7z";
-	$progressBarTotal.Value = 65
+	$progressTotal.Value=65
 	Update-Gui
 	downloadFile "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_5380_5361/strawberry-perl-5.38.0.1-64bit-portable.zip" ".\ztmp\perlportable.zip";
-	$progressBarTotal.Value = 80
-	$progressBar.Value = 0
+	$progressTotal.Value=80
+	$progressBar.Value=0
 	$info.Text=" Building zr-offline.txt... "
 	Update-Gui
 	.\ztmp\7zr.exe x -y ".\ztmp\7zExtra.7z" -o".\ztmp\"
-	$progressBarTotal.Value = 85
-	$progressBar.Value = 35
+	$progressTotal.Value=85
+	$progressBar.Value=35
 	Update-Gui
 	.\ztmp\7za.exe x -y ".\ztmp\7z2301-x64.exe" -o".\ztmp\"
-	$progressBarTotal.Value = 90
-	$progressBar.Value = 80
+	$progressTotal.Value=90
+	$progressBar.Value=80
 	Update-Gui
 	.\ztmp\7z.exe a '.\ztmp\resources.exe' '.\ztmp\winX64_1_JtR.7z' '.\ztmp\perlportable.zip' '.\ztmp\7zr.exe' '.\ztmp\7zExtra.7z' '.\ztmp\zipripper.png' -sfx'.\ztmp\7zCon.sfx' -pDependencies
-	Move-Item -Path ".\ztmp\resources.exe" -Destination ".\zr-offline.txt" -Force
-	$progressBarTotal.Value = 100
-	$progressBar.Value = 100
+	$m=([IO.File]::ReadAllLines(".\launcher.ZipRipper")).Replace('"','')
+	$fn='zr-offline.txt'
+	Move-Item -Path ".\ztmp\resources.exe" -Destination "$m$fn" -Force
+	$progressTotal.Value=100
+	$progressBar.Value=100
 	$info.Text=" Build Completed!"
 	Update-Gui
 	Remove-Item -Path ".\ztmp" -Recurse -Force
-	Sleep 3
-}
+	Sleep 1}
 
 function DownloadFile($url,$targetFile)
 {
-   $uri = New-Object "System.Uri" "$url"
-   $request = [System.Net.HttpWebRequest]::Create($uri)
+   $uri=New-Object "System.Uri" "$url"
+   $request=[System.Net.HttpWebRequest]::Create($uri)
    $request.set_Timeout(15000)
-   $response = $request.GetResponse()
-   $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
-   $responseStream = $response.GetResponseStream()
-   $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
-   $buffer = new-object byte[] 10KB
-   $count = $responseStream.Read($buffer,0,$buffer.length)
-   $downloadedBytes = $count
+   $response=$request.GetResponse()
+   $totalLength=[System.Math]::Floor($response.get_ContentLength()/1024)
+   $responseStream=$response.GetResponseStream()
+   $targetStream=New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+   $buffer=new-object byte[] 10KB
+   $count=$responseStream.Read($buffer,0,$buffer.length)
+   $downloadedBytes=$count
    while ($count -gt 0)
    {
        $targetStream.Write($buffer, 0, $count)
-       $count = $responseStream.Read($buffer,0,$buffer.length)
-       $downloadedBytes = $downloadedBytes + $count
+       $count=$responseStream.Read($buffer,0,$buffer.length)
+       $downloadedBytes=$downloadedBytes + $count
 	   $roundedPercent=[int]((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
-	   $progressBar.Value = $roundedPercent
+	   $progressBar.Value=$roundedPercent
 	   if($progressBar.Value -ne 0){$info.Text=$targetFile.Trim('"') -Replace(".*\\","");$info.Text=" Downloading " + $info.Text}
 	   if($progressBar.Value -ne $lastpercent){$lastpercent=$progressBar.Value;Update-Gui}
    }
@@ -98,12 +99,12 @@ function DownloadFile($url,$targetFile)
    $responseStream.Dispose()
 }
 
-$window2.Add_ContentRendered({
+$form.Add_ContentRendered({
 	Build
-	$window2.Close()
+	$form.Close()
 })
 
-$window2.Show()
+$form.Show()
 
 $appContext2=New-Object System.Windows.Forms.ApplicationContext
 [void][System.Windows.Forms.Application]::Run($appContext2)
