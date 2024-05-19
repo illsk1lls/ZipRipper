efa591bea04c0f5ac400a9431832b34c1c636c23
illsk1lls <gene.rovinsky@gmail.com>
2024-05-19 12:56:03 -0400

    Update for AMD GPU users
    Easier to use OpenCL installer for AMD Radeon GPU users

--- .resources/FixOpenCLforAMD.bat
+++ .resources/FixOpenCLforAMD.bat
@@ -28,23 +28,32 @@ SET "ROOTKEY32=HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\OpenCL\Vendors"
 
 echo Currently installed OpenCL Client Drivers - 64bit
 echo ==================================================
-for /f "tokens=1,*" %%A in ('reg query "%ROOTKEY64%"') do (
-    echo %%A - %%B
+for /f "tokens=1,*" %%A in ('reg query "%ROOTKEY64%" 2^>^&1') do (
+	if "%%A"=="ERROR:" (
+	echo None
+	) else (
+	echo %%A | findstr /c:".dll"
 	if /I "%%A"=="%WinDir%\System32\amdocl64.dll" SET x64Installed=1
+	)
 )
 echo/
 
 echo Currently installed OpenCL Client Drivers - 32bit
 echo ==================================================
-for /f "tokens=1,*" %%A in ('reg query "%ROOTKEY32%"') do (
-    echo %%A - %%B
+for /f "tokens=1,*" %%A in ('reg query "%ROOTKEY32%" 2^>^&1') do (
+	if "%%A"=="ERROR:" (
+	echo None
+	) else (
+	echo %%A | findstr /c:".dll"
+	)
 )
 echo/
 
 if !x64Installed! EQU 1 (
 echo AMDx64 OpenCL dll already registered...
+echo/
 echo Proceed to verify the file and register any other existing AMD OpenCL dlls..
-) ELSE (
+) else (
 echo Ready to Auto-Install/Register amdocl64.dll and any other existing AMD OpenCL dlls...
 )
 echo/
@@ -131,10 +140,18 @@ for /r %%f in (amdocl*.dll) do (
 )
 goto :eof
 
+:wait
+set /A TIMER=%~1+1
+>nul 2>&1 PING 127.0.0.1 -n %TIMER%
+exit /b
+
 :addMissingFiles
 cls
-echo AMD OpenCL x64 driver file missing, attempting to download from AMD...
-powershell -nop -c "irm 'https://download.amd.com/dir/bin/amdocl64.dll/64CB5B3Ed36000/amdocl64.dll' -o '%WinDir%\System32\amdocl64.dll'"
+<NUL set /p=AMD OpenCL x64 driver file missing...
+POWERSHELL -nop -c "$Url='https://download.amd.com/dir/bin/amdocl64.dll/64CB5B3Ed36000/amdocl64.dll';function Size-To-Human-Readable([uint64]$size){$suffix='bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB';$i = 0;while ($size -gt 1kb) {$size=$size / 1kb;$i++}"^""{0:N1} {1}"^"" -f $size, $suffix[$i]};$str_size=(Invoke-WebRequest -UseBasicParsing -Uri $Url -Method Head).Headers.'Content-Length';$tmp_size=[uint64]::Parse($str_size);$Size=Size-To-Human-Readable $tmp_size;Write-Host " Downloading 'amdocl64.dll' ($Size)";Start-BitsTransfer -Priority Foreground -Source $Url -Destination '%WinDir%\System32\amdocl64.dll'"
+echo/
+echo Download Complete.
+call :wait 2
 cls
 echo Running AMD OpenCL Driver Auto Detection
 echo ========================================
